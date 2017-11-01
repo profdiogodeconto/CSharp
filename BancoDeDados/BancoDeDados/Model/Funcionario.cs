87ce9a9eebca47e132a6dcc01c135573abc2fdc3
dideconto <digoo.deconto@up.edu.br>
2017-11-01 20:38:05 -0200

    BancoDeDados: Adicionando o modelo

---
+++ BancoDeDados/BancoDeDados/Model/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDeDados.Model
+{
+    class Funcionario
+    {
+        [Key]
+        public int FuncionarioId { get; set; }
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
